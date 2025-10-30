30f2d675e446dd1235f6f49cc78f81c970d3d5dc
Maria <maria.picapedra@gmail.com>
2025-10-30 10:09:16 -0300

    30/10 10.09

--- predictor.py
+++
@@ -1,6 +0,0 @@
-from src.predictor import PredictiveModel
-modelo = PredictiveModel()
-modelo.entrenar("data/ai4i2020.csv")
-
-from sklearn.metrics import classification_report
-print(classification_report(y_test, y_pred))
